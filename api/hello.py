53f51ba5aa2085edf30f92d0dfd73e319d03a7e2
Damon Williams <damonwilliams@MacBook-Pro.attlocal.net>
2025-05-29 16:52:23 -0700

    hello world to test python api

--- api/hello.py
+++ api/hello.py
@@ -1,5 +1,6 @@
 def handler(request):
     return {
         'statusCode': 200,
-        'body': 'Hello from Python API!'
+        'headers': {'Content-Type': 'application/json'},
+        'body': '{"message": "Hello from Python!"}'
     }
